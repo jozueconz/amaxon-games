56ae79acb851e313d535ab158c35ca2b72494609
Jozué Morales <aj.morales91@gmail.com>
2023-06-21 20:56:03 -0700

    Build switch component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { Image } from "@chakra-ui/image";
 import { HStack } from "@chakra-ui/layout";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding={"10px"}>
       <Image src={logo} boxSize={"60px"} />
+      <ColorModeSwitch />
     </HStack>
   );
 };
