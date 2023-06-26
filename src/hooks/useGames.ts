ca19e79e1164ea0ae5c5a0efdccb6de1a6c4808d
Jozué Morales <aj.morales91@gmail.com>
2023-06-25 21:33:43 -0700

    Implement React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
 import { Platform } from "./usePlatforms";
+import apiClient, { FetchResponse } from "../services/api-client";
 
 export interface Game {
   background_image: string;
@@ -11,17 +12,19 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        ordering: gameQuery.sortOrder,
-        parent_platforms: gameQuery.platform?.id,
-        search: gameQuery.searchText,
-      },
-    },
-    [gameQuery]
-  );
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient
+        .get("/games", {
+          params: {
+            genres: gameQuery.genre?.id,
+            ordering: gameQuery.sortOrder,
+            parent_platforms: gameQuery.platform?.id,
+            search: gameQuery.searchText,
+          },
+        })
+        .then((res) => res.data),
+  });
 
 export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { RAWKEY } from "../env";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
